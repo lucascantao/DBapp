47baab88725d53536daeb7c60ae8568a3ce2bf83
soldgear <soldgear@users.noreply.github.com>
2021-02-09 20:45:41 -0300

    new customer fields added, only code refactoring needed

--- README.md
+++ README.md
@@ -149,7 +149,7 @@ buttons - 'Load DB', 'Query DB', and 'Clear DB'.
 the apps operation and interface with the Customer instance will be posted. 
 -   [x] User can see a running history of notification panel messages in the log
 panel.
--   [ ] User can see a scrollable query results area where retrieved customer
+-   [x] User can see a scrollable query results area where retrieved customer
 data will be displayed.
 -   [x] User can click the 'Load DB' button to populate the database with data.
 The 'Load DB' button in your UI should be hooked to the `loadDB` event handler
@@ -161,7 +161,7 @@ results area. The 'Query DB' button in your UI should be hooked to a `queryDB`
 event handler you will add to the program.
 -   [x] User can see a message in the notification panel when the query starts
 and ends.
--   [ ] User can see a message in the query results area if there are no rows
+-   [x] User can see a message in the query results area if there are no rows
 to display.
 -   [x] User can click on the 'Clear DB' button to remove all rows from the 
 database. The 'Clear DB' button in your UI should be hooked to the `clearDB` 
@@ -171,7 +171,7 @@ operation starts and ends.
 
 ## Bonus features
 
--   [ ] User can see buttons enabled and disabled according to the following
+-   [x] User can see buttons enabled and disabled according to the following
 table.
 
     | State               | Load DB  | Query DB | Clear DB |
@@ -181,7 +181,7 @@ table.
     | Query DB clicked    | disabled | enabled  | enabled  |
     | Clear DB clicked    | enabled  | enabled  | disabled |
     
--   [ ] User can see additional Customer data fields added to those included
+-   [x] User can see additional Customer data fields added to those included
 in the code provided. Developer should add date of last order and total sales
 for the year.
 -   [ ] Developer should conduct a retrospection on this project:
--- database.js
+++ database.js
@@ -29,6 +29,7 @@ class Customer {
             };
             txn.oncomplete = (event) => {
                 notify('All rows removed!');
+                updateDatabasePanel([])
             };
             const objectStore = txn.objectStore('customers');
             const getAllKeysRequest = objectStore.getAllKeys();
@@ -117,6 +118,9 @@ class Customer {
             keys.onsuccess = (e) => {
                 
                 console.log(keys.result)
+
+                updateDatabasePanel(keys.result)
+
                 if (keys.result.length < 1) {
                     notify('No data retrived')
                 }else {
@@ -142,6 +146,8 @@ class Customer {
 
             store.createIndex('name', 'name', { unique: false })
             store.createIndex('email', 'email', { unique: true })
+            store.createIndex('date', 'date', { unique: false })
+            store.createIndex('sales', 'sales', { unique: false })
             
             console.log(store)
             db.close()
@@ -159,6 +165,9 @@ const clearDB = () => {
     log('Delete all rows from the Customers database');
     let customer = new Customer(DBNAME);
     customer.removeAllRows();
+
+    load.classList.remove('disabled')
+    clear.classList.add('disabled')
 }
 
 /**
@@ -169,17 +178,23 @@ const loadDB = () => {
 
     // Customers to add to initially populate the database with
     const customerData = [
-        { userid: '444', name: 'Bill', email: 'bill@company.com' },
-        { userid: '555', name: 'Donna', email: 'donna@home.org' }
+        { userid: '444', name: 'Bill', email: 'bill@company.com', date: '22-1-7', sales: '3621' },
+        { userid: '555', name: 'Donna', email: 'donna@home.org', date: '22-12-11', sales: '10219' }
     ];
     let customer = new Customer(DBNAME);
     customer.initialLoad(customerData);
+
+    load.classList.add('disabled')
+    clear.classList.remove('disabled')
 }
 
 const queryDB = () => {
     log('Query data')
     let customer = new Customer(DBNAME)
     customer.queryAllData()
+
+    load.classList.add('disabled')
+    clear.classList.remove('disabled')
 }
 
 function log(str) {
@@ -200,16 +215,32 @@ function Message(msg) {
 
 function updateDatabasePanel(items) {
     const DBpanel = document.querySelector('.DB-query-list')
+    const header = document.querySelector('.query-header')
+
+
     DBpanel.querySelector('.index').innerHTML = List('#')
     DBpanel.querySelector('.userid').innerHTML = List('userid')
     DBpanel.querySelector('.name').innerHTML = List('name')
     DBpanel.querySelector('.email').innerHTML = List('email')
-    items.forEach((item, index) => {
-        DBpanel.querySelector('.index').innerHTML += List(index)
-        DBpanel.querySelector('.userid').innerHTML += List(item.userid)
-        DBpanel.querySelector('.name').innerHTML += List(item.name)
-        DBpanel.querySelector('.email').innerHTML += List(item.email)
-    })
+    DBpanel.querySelector('.date').innerHTML = List('date')
+    DBpanel.querySelector('.sales').innerHTML = List('sales')
+
+    if(items.length > 0) {
+        header.classList.remove('alert')
+        header.innerHTML = ''
+        items.forEach((item, index) => {
+            DBpanel.querySelector('.index').innerHTML += List(index)
+            DBpanel.querySelector('.userid').innerHTML += List(item.userid)
+            DBpanel.querySelector('.name').innerHTML += List(item.name)
+            DBpanel.querySelector('.email').innerHTML += List(item.email)
+            DBpanel.querySelector('.date').innerHTML += List(item.date)
+            DBpanel.querySelector('.sales').innerHTML += List(item.sales)
+        })
+    }else {
+        header.classList.add('alert')
+        header.innerHTML = 'No Rows To Display'
+    }
+
 }
 
 
@@ -222,14 +253,14 @@ function notify(str) {
 const clear = document.querySelector('.btn-clear')
 const load = document.querySelector('.btn-load')
 const query = document.querySelector('.btn-query')
-const clearTerminal = document.querySelector('.clear-terminal')
+// const clearTerminal = document.querySelector('.clear-terminal')
 
 clear.addEventListener('click', clearDB)
 load.addEventListener('click', loadDB)
 query.addEventListener('click', queryDB)
 
 
-clearTerminal.addEventListener('click', () => {
-    const logPanel = document.querySelector('#log-panel')
-    logPanel.value = '#\n'
-})
+// clearTerminal.addEventListener('click', () => {
+//     const logPanel = document.querySelector('#log-panel')
+//     logPanel.value = '#\n'
+// })
